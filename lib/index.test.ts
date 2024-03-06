17981c18f5055e0f3b045455e0ca5c6b8e4bb2fa
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-03-05 20:35:26 -0500

    fix tests for ci

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -3,28 +3,26 @@ import { inspectPromise } from "./";
 
 describe("inspectPromise", () => {
 	test("pending", () => {
-		const p = new Promise(() => {})
+		const p = new Promise(() => {});
 		const details = inspectPromise(p);
 
-		expect(details.state).toBe("pending")
+		expect(details.state).toBe("pending");
 		expect(details.value).toBe(undefined);
-	})
+	});
 
 	test("fulfilled", () => {
 		const p = Promise.resolve(42);
 		const details = inspectPromise(p);
 
-		expect(details.state).toBe("fulfilled")
+		expect(details.state).toBe("fulfilled");
 		expect(details.value).toBe(42);
-	})
+	});
 
-	// Vitest will throw here; as long as it outputs "3 passed" then the error doesn't matter
 	test("rejected", () => {
-		const p = Promise.reject<Error>(new Error("hello"))
+		const p = Promise.reject<number>(42).catch(() => {});
 		const details = inspectPromise(p);
 
-		expect(details.state).toBe("rejected")
-		expect(details.value).toBeInstanceOf(Error);
-		expect(details.value!.message).toBe("hello")
-	})
-})
+		expect(details.state).toBe("rejected");
+		expect(details.value).toBe(42);
+	});
+});
